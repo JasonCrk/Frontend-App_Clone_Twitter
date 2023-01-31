1bd4054fc20707d00ab6f2c4dd93c04079252f4b
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-01-31 14:30:17 -0500

    fix(auth-layout): add background and text color

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -6,7 +6,7 @@ import { AiOutlineClose } from 'react-icons/ai'
 
 export const AuthLayout: FC = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='flex justify-center items-center h-screen bg-black text-white'>
       <div className='relative p-8 shadow-white shadow-border rounded-md'>
         <NavLink to={'/'}>
           <button className='absolute top-4 left-3 rounded-full text-white text-2xl'>
