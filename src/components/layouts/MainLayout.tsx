a77df8a87d9510d1222ead3abd42ecd894186403
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-02-05 14:45:31 -0500

    fix(mainlayout.tsx): fix responsive

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -13,8 +13,8 @@ export const MainLayout: FC = () => {
 
   return (
     <div className='bg-black'>
-      <div className='container mx-auto sm:px-0 md:px-0 lg:px-10 xl:px-30 2xl:px-40 w-full'>
-        <main className='grid max-sm:grid-cols-sm sm:grid-cols-sm max-md:grid-cols-md md:grid-cols-md lg:grid-cols-md xl:grid-cols-md 2xl:grid-cols-lg divide-x divide-neutral-600 max-md:border-r max-md:border-neutral-600 w-full'>
+      <div className='container mx-auto sm:px-0 md:px-0 lg:px-4 xl:px-30 2xl:px-40 w-full'>
+        <main className='grid max-sm:grid-cols-sm sm:grid-cols-sm md:grid-cols-md lg:grid-cols-md xl:grid-cols-xl 2xl:grid-cols-lg divide-x divide-neutral-600 max-md:border-r max-md:border-neutral-600 w-full'>
           <VerticalNavbar />
           <div className='flex flex-col text-white'>
             <Outlet />
