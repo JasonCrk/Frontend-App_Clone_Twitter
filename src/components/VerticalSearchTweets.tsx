6899466e64868222114486fcb39b738e22f131ab
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-02-06 09:33:50 -0500

    fix(verticalsearchtweets.tsx): replace input search for InputSearch component

--- src/components/VerticalSearchTweets.tsx
+++ src/components/VerticalSearchTweets.tsx
@@ -2,8 +2,7 @@ import { FC } from 'react'
 
 import { useLocation } from 'react-router-dom'
 
-import { BsSearch } from 'react-icons/bs'
-
+import { InputSearch } from './InputSearch'
 import TrendTweetsList from './TrendTweetsList'
 import MostFollowedList from './MostFollowedList'
 
@@ -16,14 +15,7 @@ const VerticalSearchTweets: FC = () => {
     <div className='text-white max-md:hidden pl-8 pt-2 w-full sticky top-0 left-0'>
       {noExplorePage && (
         <>
-          <label className='relative'>
-            <input
-              type='text'
-              className='peer/search pr-4 pl-12 py-3 placeholder:text-zinc-500 focus:outline-none focus:bg-transparent border border-transparent focus:border-blue-500 rounded-full w-full bg-zinc-900 mb-4'
-              placeholder='Search Twitter'
-            />
-            <BsSearch className='absolute top-0.5 left-4 text-lg text-zinc-500 peer-focus/search:text-blue-600' />
-          </label>
+          <InputSearch />
           <div className='bg-zinc-900 rounded-2xl w-full overflow-hidden mb-3'>
             <h6 className='px-4 py-3 text-xl font-extrabold'>Trends for you</h6>
             <TrendTweetsList />
