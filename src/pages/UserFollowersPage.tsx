50a6477da714f3d6276e600092171272ac10fa01
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-02-11 10:18:34 -0500

    feat(pages): add new subpage UserFollowersPage

---
+++ src/pages/UserFollowersPage.tsx
@@ -0,0 +1,57 @@
+import { FC, useEffect } from 'react'
+
+import { useParams } from 'react-router-dom'
+
+import { useQuery } from 'react-query'
+
+import { AxiosError } from 'axios'
+import { AccountInItem } from '../interfaces/Account'
+import { getUserFollowers } from '../services/accountService'
+
+import Spinner from '../components/Spinner'
+import AccountItem from '../components/AccountItem'
+
+export const UserFollowersPage: FC = () => {
+  const { username } = useParams() as { username: string }
+
+  const {
+    data: accounts,
+    isLoading,
+    error,
+  } = useQuery<AccountInItem[], AxiosError>(['userFollowers', username], () =>
+    getUserFollowers(username)
+  )
+
+  useEffect(() => {
+    window.scrollTo({ top: 0 })
+  }, [])
+
+  if (isLoading)
+    return (
+      <div className='flex justify-center'>
+        <Spinner />
+      </div>
+    )
+
+  if (error)
+    return (
+      <div className='text-center text-red-600 text-xl'>
+        There was a mistake
+      </div>
+    )
+
+  if (accounts?.length === 0)
+    return (
+      <div className='text-center text-xl font-bold mt-6'>
+        You don&apos;t have followers
+      </div>
+    )
+
+  return (
+    <div className='divide-y divide-outline-layout'>
+      {accounts?.map(account => (
+        <AccountItem key={account.id} {...account} />
+      ))}
+    </div>
+  )
+}
