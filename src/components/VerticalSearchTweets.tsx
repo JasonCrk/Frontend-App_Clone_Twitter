749d9965684f6d8e2ba492d09c0d9f4bc6ec00aa
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-02-14 13:01:14 -0500

    fix(verticalsearchtweets.tsx): hidden trend tweets if trends page

--- src/components/VerticalSearchTweets.tsx
+++ src/components/VerticalSearchTweets.tsx
@@ -11,16 +11,22 @@ const VerticalSearchTweets: FC = () => {
 
   const noExplorePage = location.pathname !== '/explore'
   const noSearchPage = location.pathname !== '/search'
+  const noTrendsPage = location.pathname !== '/trends'
 
   return (
     <div className='text-white max-md:hidden pl-8 pt-2 w-full sticky top-0 left-0'>
       {noExplorePage && (
         <>
           {noSearchPage && <InputSearch />}
-          <div className='bg-zinc-900 rounded-2xl w-full overflow-hidden mb-3'>
-            <h6 className='px-4 py-3 text-xl font-extrabold'>Trends for you</h6>
-            <TrendTweetsList />
-          </div>
+
+          {noTrendsPage && (
+            <div className='bg-zinc-900 rounded-2xl w-full overflow-hidden mb-3'>
+              <h6 className='px-4 py-3 text-xl font-extrabold'>
+                Trends for you
+              </h6>
+              <TrendTweetsList />
+            </div>
+          )}
         </>
       )}
 
